c9f99a47296c9c298e4ae2eefca27b5935f46431
Robson Edwards <robson.edwards@gmail.com>
2018-11-27 22:08:00 +0000

    More work

--- scripts/pulsars.R
+++ scripts/pulsars.R
@@ -1,4 +1,6 @@
 require(MASS)
+require(ggplot2)
+require(car)
 
 #### Functions ####
 logistic <- function(x) exp(x) / (1 + exp(x))
@@ -20,8 +22,11 @@ get_accuracy <- function(Y, Ypred){
 }
 
 print_model_details <- function(Y, Ypred){
-  print(get_confusion_matrix(Y, Ypred))
-  print(paste("Accuracy:", get_accuracy(Y, Ypred)))
+  confusion <- get_confusion_matrix(Y, Ypred)
+  print(round(confusion, 3))
+  print(paste("Accuracy:", round(get_accuracy(Y, Ypred), 3)))
+  print(paste("Specificity:", round(confusion[1, 1] / confusion[3, 1], 3)))
+  print(paste("Sensitivity:", round(confusion[2, 2] / confusion[3, 2], 3)))
 }
 #### Exploratory Data Analysis ####
 
@@ -30,17 +35,27 @@ summary(data)
 summary(data[data$V9==T,])
 summary(data[data$V9==F,])
 set.seed(4607)
-indices <- sample(1:nrow(data), 100, replace = F)
-plot(data[indices, ])
+indices <- sample(1:nrow(data), 1000, replace = F)
+
+# Some plots of the data
+store <- par()$xpd # Store your xpd so it can be restored later
+par(xpd = NA)
+pairs(data[indices, 1:8], col = c("orange1", "cyan")[data[indices, 9] + 1],
+       lower.panel = NULL#, panel = panel.smooth
+      )
+legend( "bottomleft", fill = c("orange1", "cyan"), 
+        legend = c("non-pulsar", "pulsar") )
+par(xpd = store) # Restore your xpd
+rm(store)
+
+# Note the collinearity issue: high correlation between (V3, V4) and (V7, V8). 
+round(cor(data), 2)
 
 # Fit a GLM with all 8 features and no interactions
 X <- cbind(rep(1, nrow(data)), data[, 1:8])
 Y <- data[, 9]
 mdl <- glm(Y~., family = binomial, data = data[, 1:8])
 summary(mdl)
-
-#linpred <- as.matrix(X) %*% mdl$coefficients
-#nlpred <- logistic(linpred)
 
 # Accuracy and Confusion Matrix for this model as a classifier 
 #   with cutoff p = 0.5
@@ -75,21 +90,29 @@ points(cutoff, accuracy(cutoff))
 text(0.4, 0.95, paste(round(cutoff, 3), ", ", round(accuracy(cutoff), 3), 
                       sep = ""))
 
-# Fitting smaller GLMs by removing V7 and/or V8 
+# Fitting smaller GLMs by removing some of V3, V7, V8 
+#   Removing V8 gives a model with AIC 2634.3
 mdl2 <- glm(Y~., family = binomial, data = data[,1:7])
 summary(mdl2)
+#   Removing V7 gives a model with AIC 2632.1
 mdl3 <- glm(Y~., family = binomial, data = data[,c(1:6, 8)])
 summary(mdl3)
+#   Removing V7 AND V8 gives AIC 2640.7. We have gone too far. 
 mdl4 <- glm(Y~., family = binomial, data = data[,1:6])
 summary(mdl4)
+#   Removing V3 and 
+mdl5 <- glm(Y~.-V3-V7, family = binomial, data = data[,1:8])
+summary(mdl5)
 
 # Fitting larger GLMs by adding interaction terms
 mdl5 <- glm(Y~.*., family = binomial, data = data[,1:8])
 summary(mdl5)
 
-mdl6 <- glm(Y~.+V1*V2+V1*V3+V2*V3+V2*V4+V5*V6+V6*V7 ,
+# Only significant interaction terms from the above: AIC 2478
+mdl6 <- glm(Y~.+V1*V2+V1*V3+V2*V3+V2*V4+V5*V6+V6*V7,
             family = binomial, data = data[,1:8])
 summary(mdl6)
 
-mdl7 <- stepAIC(mdl5, direction = "both",  trace = T)
+
+mdl7 <- stepAIC(mdl5, direction = "both",  trace = F)
 summary(mdl7)
